291657c038bd5fa2358173871bd7692d95cc7376
Mike Hartington <mikehartington@gmail.com>
2020-05-20 13:39:14 -0400

    add sounds

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,24 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+} from '@ionic/react';
+import React, { useRef } from 'react';
 import './Home.css';
 
 const Home: React.FC = () => {
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const playAudio = async (fileName: string) => {
+    if (audioRef.current) {
+      audioRef.current.pause();
+      audioRef.current.currentTime = 0;
+      audioRef.current.src = `/assets/mp3s/${fileName}`;
+      audioRef.current.play();
+    }
+  };
   return (
     <IonPage>
       <IonHeader>
@@ -12,12 +27,10 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <IonButton onClick={() => playAudio('Crowd-Applause-2.mp3')}>Clap</IonButton>
+        <IonButton onClick={() => playAudio('Crowd-Boo.mp3')}>Boo</IonButton>
+        <IonButton onClick={() => playAudio('Crowd-Laughter-3.mp3')}>Laughter</IonButton>
+        <audio ref={audioRef}></audio>
       </IonContent>
     </IonPage>
   );
